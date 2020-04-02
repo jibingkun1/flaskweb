33dd864f7e90131a993c9c6699c105f11dee0718
Bruce <1046244623@qq.com>
2020-04-02 09:52:37 +0800

    模板

--- day1/watchlist/app.py
+++ day1/watchlist/app.py
@@ -1,4 +1,4 @@
-from flask import Flask,url_for
+from flask import Flask,url_for,render_template
 
 app = Flask(__name__)
 
@@ -8,7 +8,19 @@ app = Flask(__name__)
 # def index():
 #     return "<h1>Hello Flask llaall </h1>"
 
-@app.route('/user/<name>')
-def index(name):
-    print(url_for('index',name="hahaha"))
-    return "<h1>Hello %s </h1>"%name
+@app.route('/')
+def index():
+   name = "Bruce"
+   movies = [
+       {'title':"大赢家","year":"2020"},
+       {'title':"囧妈","year":"2020"},
+       {'title':"疯狂外星人","year":"2019"},
+       {'title':"战狼","year":"2017"},
+       {'title':"速度与激情8","year":"2018"},
+       {'title':"极限特工","year":"2010"},
+       {'title':"叶问","year":"2014"},
+       {'title':"杀破狼","year":"2000"},
+       {'title':"叶问2","year":"2016"},
+   ]
+
+   return render_template('index.html',name=name,movies=movies)
