25e9cec41bf729d555d705e74d1866cc5bead661
Bruce <1046244623@qq.com>
2020-04-03 14:59:40 +0800

    模板继承和错误处理函数和模板上下文函数

--- day1/watchlist/app.py
+++ day1/watchlist/app.py
@@ -29,10 +29,8 @@ class Movie(db.Model):
 # views 视图函数
 @app.route('/')
 def index():
-
-   user = User.query.first()  #查询出来用户记录
-   movies = Movie.query.all() 
-   return render_template('index.html',user=user,movies=movies)
+    movies = Movie.query.all()
+    return render_template('index.html',movies=movies)
 
 
 # 自定义命令
@@ -69,7 +67,13 @@ def forge():
     click.echo("导入数据完成")
 
 
+# 错误处理函数
 @app.errorhandler(404)
 def page_not_found(e):
+    return render_template('404.html')
+
+# 模板上下文处理函数
+@app.context_processor
+def common_user():
     user = User.query.first()
-    return render_template('404.html',user=user)
+    return dict(user=user)
