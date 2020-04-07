0b83962aca7a6fa3ea7b70524b64f98286f4a6e8
Bruce <1046244623@qq.com>
2020-04-07 14:46:14 +0800

    表单操作完成

--- day1/watchlist/app.py
+++ day1/watchlist/app.py
@@ -48,6 +48,32 @@ def index():
     movies = Movie.query.all()
     return render_template('index.html',movies=movies)
 
+# 更新/movie/edit
+@app.route('/movie/edit/<int:movie_id>',methods=['GET','POST']) 
+def edit(movie_id):
+    movie = Movie.query.get_or_404(movie_id)
+    if request.method == 'POST':
+        title = request.form['title']
+        year = request.form['year']
+        if not title or not year or len(year)>4 or len(title)>60:
+            flash('输入有误')
+            return redirect(url_for('edit'),movie_id = movie_id)
+        movie.title = title
+        movie.year = year
+        db.session.commit()
+        flash('电影更新完成')
+        return redirect(url_for('index'))
+    return render_template('edit.html',movie=movie)
+
+# delete视图函数
+@app.route('/movie/delete/<int:movie_id>',methods=['GET','POST']) 
+def delete(movie_id):
+    movie = Movie.query.get_or_404(movie_id)
+    db.session.delete(movie)
+    db.session.commit()
+    flash('删除完成')
+    return redirect(url_for('index'))
+
 
 # 自定义命令
 # 建立空数据库
