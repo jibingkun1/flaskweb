ed86a499ea2235e8238b3f794d4410b3ff5016f6
Bruce <1046244623@qq.com>
2020-04-09 08:29:27 +0800

    删除day1

--- watchlist/watchlist/__init__.py
+++ watchlist/watchlist/__init__.py
@@ -4,7 +4,6 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
-
 app = Flask(__name__)
 WIN = sys.platform.startswith('win')
 if WIN:
@@ -13,7 +12,7 @@ else:
     prefix = "sqlite:////" #Mac，Linux平台
 app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(os.path.dirname(app.root_path),'data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = '1903_dev'
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY','dev')
 
 db = SQLAlchemy(app)
 
